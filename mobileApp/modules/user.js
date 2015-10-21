154f4bfc6b25a4702c8e7a178205a45f72f36303
Erickson Juang <a820224@gmail.com>
2015-10-22 05:36:09 +0800

    link spider - login & getProfile

--- mobileApp/modules/user.js
+++ mobileApp/modules/user.js
@@ -1,12 +1,41 @@
-var PythonShell = require('python-shell');
+var PyScript = require(__MobileAppBase + 'mobileApp/modules/runPython');
+var Logging = require(__MobileAppBase + 'modules/logging')('mobileAPI');
 
-var PyOptions = {
-  mode: 'json',
-  scriptPath: '../pyScript/',
-  args: ['QAQ', 'test']
+module.exports.login = function(req, res, next) {
+
+  Logging.writeMessage('Access mobileApp/user/login from ' + req.ip ,'access')
+  loginPortal(req.body.username, req.body.password, function(r) {
+    console.log(r)
+    res.send('user/login api')
+    Logging.writeMessage('response to (mobileApp/user/login) ' + req.ip ,'access')
+  })
+
 }
 
-PythonShell.run('user.py', PyOptions, function (err, results) {
-  console.log(results[0].status)
-  // results is an array consisting of messages collected during execution
-});
+module.exports.profile = function(req, res, next) {
+  Logging.writeMessage('Access mobileApp/user/profile from ' + req.ip ,'access')
+  getProfile(req.body.username, req.body.password, function(r) {
+    console.log(r)
+    res.send('user/profile api')
+    Logging.writeMessage('response to (mobileApp/user/profile) ' + req.ip ,'access')
+  })
+}
+
+
+var isRegister = function() {
+
+}
+
+var loginPortal = function(username, password, next) {
+  PyScript({
+    args: ['login', username, password],
+    scriptFile: 'user.py'
+  }, next)
+}
+
+var getProfile = function(username, password, next) {
+  PyScript({
+    args: ['getProfile', username, password],
+    scriptFile: 'user.py'
+  }, next)
+}
--- mobileApp/router/user.js
+++ mobileApp/router/user.js
@@ -1,6 +1,8 @@
-var user = require('./mobileApp/modules/user');
+var user = require( __MobileAppBase + 'mobileApp/modules/user');
 var express = require('express');
 var router = express.Router();
 
 router.post('/login', user.login);
-router.get('/porfile', user.porfile);
+router.post('/profile', user.profile);
+
+module.exports = router;
