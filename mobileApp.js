e9f91987f17071bd328ec7460335eeab967f861b
Erickson Juang <a820224@gmail.com>
2015-10-23 05:17:18 +0800

    log ips and path when 404 not found

--- mobileApp.js
+++ mobileApp.js
@@ -33,7 +33,7 @@ app.use('/user', UserRouter);
 // catch 404 and forward to error handler
 // next 疑似是擺好看的，官方文件沒說什麼東東會被傳進來
 app.use(function(req, res, next) {
-  Logging.writeMessage('404 Not Found' + req, 'error')
+  Logging.writeMessage('request('+req.path+') 404 Not Found from' + req.ips, 'error')
   res.status(404).json({
     status: 404,
     mes: 'Not Found'
