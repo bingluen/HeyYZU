54404730e378cd476ae5fb7485e25573dfc4ae62
Erickson Juang <a820224@gmail.com>
2015-10-29 14:56:51 +0800

    implement login expect token creation

--- mobileApp/modules/user.js
+++ mobileApp/modules/user.js
@@ -4,50 +4,165 @@ var Database = require(__MobileAppBase + 'modules/database');
 var RSA = require(__MobileAppBase + 'modules/rsa');
 
 module.exports.login = function(req, res, next) {
-  Logging.writeMessage('Access mobileApp/user/login from ' + req.ip ,'access')
-  loginPortal(req.body.username, req.body.password, function(r) {
-    console.log(r)
-    res.send('user/login api')
-    Logging.writeMessage('response to (mobileApp/user/login) ' + req.ips ,'access')
-  })
+  var privateRSA = RSA('private')
+  /* ==== 測試用 ==== 
+  var publicRSA = RSA();
 
+  var testdata = {
+    username: '',
+    password: ''
+  }
+
+  var encryptPrivate = privateRSA.encryptPrivate(JSON.stringify(testdata), 'base64', 'utf-8');
+  var encryptPublic = publicRSA.encrypt(JSON.stringify(testdata),'base64', 'utf-8');
+
+  console.log('========密文是=========')
+  console.log(encryptPublic);
+  console.log('========解密後=========')
+  console.log(privateRSA.decrypt(encryptPublic, 'utf-8'))
+  /* ===== 測試用END ==== */
+
+  var userData = {}
+
+  //portal帳號密碼檢驗器
+  var loginPortal = function(next) {
+    PyScript({
+      args: ['login', userData.username, userData.password],
+      scriptFile: 'user.py'
+    }, next)
+  }
+
+  //從portal 爬user profile
+  var getProfile = function(next) {
+    PyScript({
+      args: ['getProfile', userData.username, userData.password],
+      scriptFile: 'user.py'
+    }, next)
+  }
+
+  //Token 產生 function
+  var createToken = function() {
+    console.log(userData)
+  }
+
+  //註冊確認
+  var isRegister = function() {
+    var queryStatment = 'SELECT id, portalUsername, portalPassword, accessToken From user Where portalUsername = ?';
+    var params = [];
+    params.push(userData.username);
+    params.push(userData.password);
+    Database.query(queryStatment, params, function(err, row, field) {
+      if (err) {
+        Logging.writeMessage('[Response][DatabaseError]['+ req.ip +']path:user/login. DetailIp{ '+ req.ips + ' }','access')
+        res.status(1004).json({
+          state: 'Internal error',
+          messages: 'Internal error',
+          statusCode: 1004
+        })
+        return;
+      }
+      if (row.length != 1) {
+        doRegister()
+      } else {
+        userData = row[0]
+        createToken()
+      }
+    });
+  }
+
+  //進行註冊
+  var doRegister = function() {
+    var queryStatment = 'INSERT INTO user (chiName, engName, portalUsername, portalPassword, cellphone, email, gender, birth) Value (?, ?, ?, ?, ?, ?, ?, ?)';
+
+    getProfile(function(r) {
+      if (r.status.statusCode == 1002) {
+        resM = {}
+        resM.state = r.status.state;
+        resM.messages = r.status.messages;
+        resM.statusCode = r.status.statusCode;
+        res.status(resM.statusCode).json(resM);
+        Logging.writeMessage('[Response][loginFail]['+ req.ip +']path:user/login. DetailIp{ '+ req.ips + ' }','access')
+        return;
+      }
+      var params = [];
+      userData = r.profile;
+      params.push(userData.chiName);
+      params.push(userData.engName);
+      params.push(userData.username);
+      params.push(userData.password);
+      params.push(userData.cellphone);
+      params.push(userData.email);
+      params.push((userData.gender == '\u7537' ? 1 : 0));
+      params.push(userData.birth);
+      Database.query(queryStatment, params, function(err, row, field) {
+        if(err) {
+          Logging.writeMessage('[Response][DatabaseError]['+ req.ip +']path:user/login. DetailIp{ '+ req.ips + ' }','access')
+          res.status(1004).json({
+            state: 'Internal error',
+            messages: 'Internal error',
+            statusCode: 1004
+          })
+          return;
+        } else {
+          createToken()
+        }
+      })
+    })
+  }
+
+  //login callback
+  var loginCallback = function(r) {
+
+    if(r.status.statusCode === 1001) {
+      //登入成功
+      isRegister();
+    } else if(r.status.statusCode === 1002) {
+      //登入失敗
+      resM = {}
+      resM.state = r.status.state;
+      resM.messages = r.status.messages;
+      resM.statusCode = r.status.statusCode;
+      res.status(resM.statusCode).json(resM);
+      Logging.writeMessage('[Response][loginFail]['+ req.ip +']path:user/login. DetailIp{ '+ req.ips + ' }','access')
+    }
+  }
+
+  //紀錄Log
+  Logging.writeMessage('[Request]['+ req.ip +']path:user/login. DetailIp{ '+ req.ips + ' }' ,'access')
+
+  //檢驗資料欄位，messages不存在，直接退回
+  if(!req.body.messages) {
+    Logging.writeMessage('[Response][ParamsError]['+ req.ip +']path:user/login. DetailIp{ '+ req.ips + ' }','access')
+    res.status(1003).json({
+      state: 'ParamsError',
+      messages: 'Params can not be read',
+      statusCode: 1003
+    })
+    return;
+  }
+
+  //解碼後解析為JSON
+  var params = JSON.parse(privateRSA.decrypt(req.body.messages));
+
+  //檢查帳號密碼是否存在
+  if(!params.username || !params.username.match(/^s(9[0-9]|1[0-9]{2})[0-9]{4}$/g) || !params.password) {
+    Logging.writeMessage('[Response][ParamsError]['+ req.ip +']path:user/login. DetailIp{ '+ req.ips + ' }','access')
+    res.status(1003).json({
+      state: 'ParamsError',
+      messages: 'Params can not be read',
+      statusCode: 1003
+    })
+    return;
+  } else {
+    userData.username = params.username;
+    userData.password = params.password;
+    loginPortal(loginCallback)
+  }
 }
 
 module.exports.profile = function(req, res, next) {
   Logging.writeMessage('Access mobileApp/user/profile from ' + req.ips ,'access')
   getProfile(req.body.username, req.body.password, function(r) {
-    console.log(r)
-    res.send('user/profile api')
     Logging.writeMessage('response to (mobileApp/user/profile) ' + req.ips ,'access')
   })
-}
-
-
-var isRegister = function(userData, next) {
-  var queryStatment = 'SELECT id, portalUsername, protalPassword, accessToken Where portalUsername = ?';
-  var params = userData.username;
-  database.query(queryStatment, params, function(err, row, field) {
-    if (row.length != 1) {
-      next(false)
-    } else {
-      next(true)
-    }
-  });
-}
-
-var doRegister = function(userData, next) {
-}
-
-var loginPortal = function(username, password, next) {
-  PyScript({
-    args: ['login', username, password],
-    scriptFile: 'user.py'
-  }, next)
-}
-
-var getProfile = function(username, password, next) {
-  PyScript({
-    args: ['getProfile', username, password],
-    scriptFile: 'user.py'
-  }, next)
 }
