2508c2205fd034084970286a9325baddcf456d8e
Erickson Juang <a820224@gmail.com>
2015-12-25 19:05:11 +0800

    add Tool module
    	extract getYearNow & getSemesterNow function to Tool

---
+++ mobileApp/modules/tool.js
@@ -0,0 +1,16 @@
+module.exports.getYearNow = function()
+{
+  var nowTime = new Date(Date.now());
+  var year = nowTime.getYear() - 11;
+  if(year <= 7) return year - 1;
+  else return year;
+}
+
+module.exports.getSemesterNow = function()
+{
+  var nowTime = new Date(Date.now());
+  var month = nowTime.getMonth();
+
+  if(month <= 7 && month >= 2) return 2;
+  else return 1;
+}
