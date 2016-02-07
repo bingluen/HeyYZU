692ad01669e295f61014df2c88a7033712aa5041
Erickson Juang <a820224@gmail.com>
2016-02-08 05:57:32 +0800

    Implement student login:
    	The following item is done
    	+ check params is existed beforce verify account

--- mobileAPI/controller/v2/loginController.js
+++ mobileAPI/controller/v2/loginController.js
@@ -6,16 +6,32 @@ module.exports.student = function(req, res, next) {
     user
   ;
 
-  //Step 1 check messages is exists
+  /*
+    Step 1 check messages is exists
+   */
   if (!(req.body.messages && (messages = JSON.parse(rsa.priDecrypt(req.body.messages))))) {
     res.status(200).json({
       statusCode: 1101,
       status: 'Params illegal'
-    })
+    });
     return;
   }
 
-  //Step 2 verify username and get profile
+  /*
+    Step 2 check params is exists
+   */
+  if(!messages.username || !messages.password ||
+    !messages.deviceId || !messages.deviceType) {
+    res.status(200).json({
+      statusCode: 1101,
+      status: 'Params illegal'
+    });
+    return;
+  }
+
+  /*
+    Step 3 verify account
+   */
   pyCarriers({
     args: ['login', messages.username, messages.password],
     scriptFile: 'catalyst.py'
@@ -24,14 +40,16 @@ module.exports.student = function(req, res, next) {
       res.status(200).json({
         statusCode: r.statusCode,
         status: r.status
-      })
+      });
       return;
     } else {
       user = {
         username: messages.username,
-        password: messages.password
+        password: messages.password,
+        deviceId: messages.deviceId,
+        deviceInfo: messages.deviceInfo,
       }
     }
   })
-
+g
 }
