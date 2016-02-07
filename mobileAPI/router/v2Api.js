920c067be872421b36a853db5f1b4af4d5f4c2a0
Erickson Juang <a820224@gmail.com>
2016-02-07 16:26:43 +0800

    setting student login port

--- mobileAPI/router/v2Api.js
+++ mobileAPI/router/v2Api.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/', function(res, req, next) {
-  res.redirect('https://hey.yzu.us');
-});
+/* Controller */
+var loginController = require( __mobileAPIBase + 'controller/loginController');
+
+/*
+  Login Router
+ */
+router.post('/login/student', loginController.student);
 
 module.exports = router;
