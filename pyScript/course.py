b8c07f14b59f8982a40c8d00e54a81a6144213db
Erickson Juang <a820224@gmail.com>
2016-03-06 13:11:59 +0800

    implement get course notice

--- pyScript/course.py
+++ pyScript/course.py
@@ -5,9 +5,13 @@ import re
 import sys
 from login import loginPortal
 import json
+from HTMLParser import HTMLParser
 
 URL_FIRST_TO_PAGE = 'https://portalx.yzu.edu.tw/PortalSocialVB/FPage/FirstToPage.aspx?'
 URL_HOMEWORK_PAGE = 'https://portalx.yzu.edu.tw/PortalSocialVB/THom/HomeworkList.aspx?Menu=Hom'
+URL_NOTICE_PAGE = 'https://portalx.yzu.edu.tw/PortalSocialVB/FMain/PostWall.aspx?Menu=New'
+URL_NOTICE_LIST = 'https://portalx.yzu.edu.tw/PortalSocialVB/FMain/PostWall.aspxGetPostWall'
+URL_NOTICE_INNER = 'https://portalx.yzu.edu.tw/PortalSocialVB/FMain/PostWall.aspx/divParentInnerHtml'
 
 
 class Course(loginPortal):
@@ -26,7 +30,7 @@ class Course(loginPortal):
                 self.messages['data'].append(result['data'])
                 self.messages['statusCode'] = result['statusCode']
                 self.messages['status'] = result['status']
-            else:
+            elif argSource == 'infile':
                 with open(args) as f:
                     courseList = json.loads(f.read())['args']
                 for course in courseList:
@@ -34,6 +38,29 @@ class Course(loginPortal):
                     self.messages['data'].append(result['data'])
                     self.messages['statusCode'] = result['statusCode']
                     self.messages['status'] = result['status']
+            else:
+                self.messages['statusCode'] = 502
+                self.messages['status'] = 'Params error: no arg source assign'
+            return
+        if task == 'notice':
+            notice = Notice(self.request)
+            self.messages['data'] = []
+            if argSource == 'inline':
+                result = notice.getNoticeList(args)
+                self.messages['data'].append(result['data'])
+                self.messages['statusCode'] = result['statusCode']
+                self.messages['status'] = result['status']
+            elif argSource == 'infile':
+                with open(args) as f:
+                    courseList = json.loads(f.read())['args']
+                for course in courseList:
+                    result = notice.getNoticeList(args)
+                    self.messages['data'].append(result['data'])
+                    self.messages['statusCode'] = result['statusCode']
+                    self.messages['status'] = result['status']
+            else:
+                self.messages['statusCode'] = 502
+                self.messages['status'] = 'Params error: no arg source assign'
             return
         else:
             self.messages['statusCode'] = 502
@@ -146,3 +173,80 @@ class Homework:
             uploadDetail['id'] = parameter[0][2]
 
             return uploadDetail
+
+class Notice:
+    def __init__(self, requests):
+        self.request = requests
+
+    def getNoticeList(self, args = None):
+        if args is None:
+            return {
+                'statusCode': 502,
+                'status': 'Params error: no lesson assign'
+            }
+        self.request.get(URL_FIRST_TO_PAGE + 'y=' + str(args['year']) + '&s=' + str(args['semester']) + '&id=' + args['courseCode'] + '&c=' + args['class'])
+
+        noticelist = []
+
+        #Page 1
+        content = BeautifulSoup(self.request.get(URL_NOTICE_PAGE).text, 'lxml')
+        posts = content.find_all(class_='PanelPost')
+        #pages = self.getPages(content.find(class_='divPageNum'))
+        noticelist = noticelist + self.parse(posts)
+
+        return {
+        'data': {
+            'year': args['year'],
+            'semester': args['semester'],
+            'courseCode': args['courseCode'],
+            'class': args['class'],
+            'noticelist': noticelist
+        },
+        'statusCode': 200,
+        'status': 'get notice list successful.'
+        }
+
+    def parse(self, posts):
+        postList = []
+        for post in posts:
+            detail = post.find_all('td')
+            #if len(re.findall(u'【(教材|作業)】', detail[2].text, re.S)) > 0:
+            #if len(re.findall(u'【作業】', detail[2].text, re.S)) > 0:
+            #    continue
+            postContent = {}
+            try:
+                postContent['portalId'] = re.findall('ShowPostGridUnique\(([0-9]+),[0-1]\)' ,detail[1].a['href'], re.S)
+                postContent['author'] = post.find('img', class_='imgPostAuthor')['title']
+                postContent['title'] = re.sub(u'【.*】|\([0-9]{4}_[A-Z]{2}[0-9]{3}_[A-Z]\)', '', detail[2].text)
+                postContent['date'] = detail[4].text
+                postContent['content'] = self.getNewsContent(postContent['portalId'])
+                postContent['attach'] = self.parseAttach()
+                postList.append(postContent)
+            except Exception,e:
+                print 'error',e
+                #print(post)
+        return postList
+
+    def getNewsContent(self, uid):
+        headers = {
+            'content-type': 'application/json',
+        }
+        data = {
+            'ParentPostID': uid,
+            'pageShow': 0
+        }
+
+        content = self.request.post(URL_NOTICE_INNER, data=json.dumps(data), headers=headers)
+        self.newsContent = content
+        return HTMLParser.unescape(BeautifulSoup(json.loads(content)['d'], 'lxml').find('textarea').text)
+
+    def parseAttach(self):
+        if BeautifulSoup(json.loads(self.newsContent)['d'], 'lxml').find('tr').find('a').img is not None:
+            url = BeautifulSoup(json.loads(self.newsContent)['d'], 'lxml').find('tr').find('a')['href']
+            return {
+                'CourseType': re.findall('CourseType=([0-9])', url, re.S)[0],
+                'AttachmentID': re.findall('AttachmentID=([0-9]+)', url, re.S)[0],
+                'AttachmentFileName': re.findall('AttachmentFileName=(.+)', url, re.S)[0]
+            }
+        else:
+            return {}
