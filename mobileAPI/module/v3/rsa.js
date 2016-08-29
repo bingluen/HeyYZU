dfd64b309138eb590cddd219c597f4985d188b52
Erickson Juang <a820224@gmail.com>
2016-08-29 19:44:04 +0800

    implement rsa v3
    Different between v3 and v2:
    Once occure error during decrypt/encrypt will throw exception (v3) instead of return false (v2)

---
+++ mobileAPI/module/v3/rsa.js
@@ -0,0 +1,46 @@
+var ursa = require('ursa');
+var fs = require('fs-extra');
+
+var privateKey = fs.readFileSync(__mobileAPIBase + __mobileAPIConfig.v2.RSA.private);
+var publicKey = fs.readFileSync(__mobileAPIBase + __mobileAPIConfig.v2.RSA.public);
+
+var priRSA = ursa.createPrivateKey(privateKey);
+var pubRSA = ursa.createPublicKey(publicKey);
+
+
+function RsaException(statusCode, status) {
+  this.statusCode = statusCode;
+  this.status = status;
+}
+
+module.exports.pubEncrypt = function(str) {
+  try {
+    return pubRSA.encrypt(str, 'utf8' , 'base64', ursa.RSA_PKCS1_PADDING);
+  } catch (e) {
+    throw new RsaException(1203, "Fail to encrypt.")
+  }
+}
+
+module.exports.pubDecrypt = function(hash) {
+  try {
+    return pubRSA.publicDecrypt(hash, 'base64' , 'utf8');
+  } catch (e) {
+    throw new RsaException(1104, "Fail to decrypt.")
+  }
+}
+
+module.exports.priEncrypt = function(str) {
+  try {
+    return priRSA.privateEncrypt(str, 'utf8', 'base64');
+  } catch (e) {
+    throw new RsaException(1203, "Fail to encrypt.")
+  }
+}
+
+module.exports.priDecrypt = function(hash) {
+  try {
+    return priRSA.decrypt(hash, 'base64', 'utf8', ursa.RSA_PKCS1_PADDING);
+  } catch (e) {
+    throw new RsaException(1104, "Fail to decrypt.")
+  }
+}
