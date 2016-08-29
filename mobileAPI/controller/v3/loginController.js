f97665fedbfaeb4975ada89005787d6b98c51f96
Erickson Juang <a820224@gmail.com>
2016-08-29 20:17:29 +0800

    implement basic params check
    1. req.body.messages check
    2. rsa decrypt check
    3. JSON parse check

--- mobileAPI/controller/v3/loginController.js
+++ mobileAPI/controller/v3/loginController.js
@@ -1,8 +1,31 @@
+var rsaModule = require(__mobileAPIBase + 'module/v3/rsa');
+
 module.exports = {
     student: (req, res, next) => {
-      res.status(404).json({
-        statusCode: 404,
-        status: "Not found."
-      });
+        // check request body
+        if (!req.body.messages) {
+            res.ststus(400).json({
+                statusCode: 1101,
+                status: "Params illegal."
+            });
+            return;
+        }
+
+        // try to decrypt messages
+        var messages;
+        try {
+            messages = rsaModule.priDecrypt(req.body.messages)
+        } catch (e) {
+            res.ststus(400).json(JSON.stringify(e));
+            return;
+        }
+
+        // try parse JSON string
+        try {
+            messages = JSON.parse(messages)
+        } catch (e) {
+            res.status(400).json(JSON.stringify(e));
+            return;
+        }
     }
 }
