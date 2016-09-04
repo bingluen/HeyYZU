9896e185d5eb1703d578eec7e4db4e9e2961be1c
Erickson Juang <a820224@gmail.com>
2016-09-04 17:43:32 +0800

    implement library search

--- mobileAPI/controller/v3/libraryController.js
+++ mobileAPI/controller/v3/libraryController.js
@@ -1,38 +1,81 @@
+"use strict"
+
+const libraryModel = require(__mobileAPIBase + 'module/v3/library');
+
+function getProp(obj, prop) {
+  if (obj.hasOwnProperty(prop)) return obj[prop];
+  else throw new ReferenceError('The value `' + prop +
+    '` is not exist');
+}
+
 module.exports = {
-    doSearch: (req, res, next) => {
-      res.status(404).json({
-        statusCode: 404,
-        status: "Not found."
-      });
-    },
-    getBookInfo: (req, res, next) => {
-      res.status(404).json({
-        statusCode: 404,
-        status: "Not found."
-      });
-    },
-    getLoanRecord: (req, res, next) => {
-      res.status(404).json({
-        statusCode: 404,
-        status: "Not found."
-      });
-    },
-    getCollection: (req, res, next) => {
-      res.status(404).json({
-        statusCode: 404,
-        status: "Not found."
-      });
-    },
-    add2Collection: (req, res, next) => {
-      res.status(404).json({
-        statusCode: 404,
-        status: "Not found."
-      });
-    },
-    removeFromCollection: (req, res, next) => {
-      res.status(404).json({
-        statusCode: 404,
-        status: "Not found."
-      });
+  doSearch: (req, res, next) => {
+    let key = {};
+    try {
+      key.type = getProp(req.query, "keyType");
+      key.keyword = getProp(req.query, "keyword");
+    } catch (e) {
+      if (e instanceof ReferenceError) {
+        res.status(400).json({
+          statusCode: 1101,
+          status: "Params illegal.",
+          error: e.toString().replace("ReferenceError: ", ""),
+          debug: req.debug
+        });
+        return;
+      }
     }
+
+    libraryModel
+      .search(key.type, key.keyword)
+      .then(
+        (resolveTask) => {
+          res.status(200).json({
+            statusCode: 200,
+            status: "successful.",
+            data: resolveTask,
+            debug: req.debug,
+          });
+        },
+        (rejectTask) => {
+          res.status(rejectTask.httpStatus).json({
+            statusCode: rejectTask.statusCode,
+            status: rejectTask.status,
+            error: rejectTask.error,
+            debug: req.debug,
+          });
+        }
+      )
+
+  },
+  getBookInfo: (req, res, next) => {
+    res.status(404).json({
+      statusCode: 404,
+      status: "Not found."
+    });
+  },
+  getLoanRecord: (req, res, next) => {
+    res.status(404).json({
+      statusCode: 404,
+      status: "Not found."
+    });
+  },
+  getCollection: (req, res, next) => {
+    res.status(404).json({
+      statusCode: 404,
+      status: "Not found."
+    });
+  },
+  add2Collection: (req, res, next) => {
+    res.status(404).json({
+      statusCode: 404,
+      status: "Not found."
+    });
+  },
+  removeFromCollection: (req, res, next) => {
+    res.status(404).json({
+      statusCode: 404,
+      status: "Not found."
+    });
+  }
 }
---
+++ mobileAPI/module/v3/library.js
@@ -0,0 +1,79 @@
+"use strict"
+
+const request = require('request')
+
+const KEY_TYPE_ERROR = { httpStatus: 400, statusCode: 2301, status: "KeyType error.", error: "KeyType error."};
+const REQUEST_ERROR = { httpStatus: 500, statusCode: 1301, status: "Request error."};
+const REQUEST_INTENRAL_ERROR = {httpStatus: 500, statusCode: 1304, status: "Library server internal error."};
+const SEARCH_API = "http://unipop.yzu.edu.tw/OpenAPI/api/lib/keyword/";
+
+function LibraryException(errorObject) {
+  this.httpStatus = errorObject.httpStatus;
+  this.statusCode = errorObject.statusCode;
+  this.status = errorObject.status;
+  this.error = errorObject.error;
+}
+
+module.exports = {
+  search: (keyType, key) => {
+    let legalKey = [
+      'title_begin', 'title', 'author', 'publisher',
+      'serials', 'subject', 'ISBN', 'ISSN'
+    ];
+
+    if (legalKey.indexOf(keyType) < 0) {
+      throw new LibraryException(KEY_TYPE_ERROR);
+    }
+
+    let doSearch = new Promise((resolve, reject) => {
+      let dataStream = ""
+      request
+        .get(SEARCH_API + keyType + '=' + encodeURI(key))
+        .on('response', (response) => {
+          response
+            .on('data', (chunk) => {
+              dataStream += chunk;
+            })
+            .on('end', () => {
+              resolve(dataStream)
+            })
+          ;
+        })
+        .on('error', (error) => {
+          reject(error);
+        })
+      ;
+    });
+
+    return doSearch
+      .then((resolveTask) => {
+        // re-parse
+        try {
+          resolveTask = JSON.parse(resolveTask);
+        } catch (e) {
+          throw new LibraryException(REQUEST_INTENRAL_ERROR);
+        }
+        console.log(resolveTask);
+        // adjust structure
+        resolveTask = resolveTask.map((cv) => ({
+          bibliosno: cv.bibliosno,
+          title: cv.bktitle,
+          author: cv.author,
+          publishYear: parseInt(cv.publish_YY.replace(/[^0-9]/g, ''), 10) || -1,
+          callNo: cv.ccl.replace(/<br>| /g, ''),
+          type: cv.material_type,
+          ISBN: parseInt(cv.ISBN.replace(/[^0-9]/g, ''), 10) || -1,
+          ISSN: parseInt(cv.ISSN.replace(/[^0-9]/g, ''), 10) || -1,
+          cover: cv.Cover,
+          publish: cv.Publish
+        }));
+        resolveTask.sort((a, b) => (b.publishYear - a.publishYear));
+        return resolveTask;
+      })
+      .then(
+      (resolveTask) => {
+        return resolveTask;
+      }
+    );
+  }
+}
