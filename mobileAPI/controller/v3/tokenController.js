72159714dc7dcc802b1e155839768e8a1704401f
Erickson Juang <a820224@gmail.com>
2016-09-07 11:19:12 +0800

    implement api for verify token

--- mobileAPI/controller/v3/tokenController.js
+++ mobileAPI/controller/v3/tokenController.js
@@ -1,9 +1,19 @@
+"use strict"
+const tokenModule = require(__mobileAPIBase + 'module/v3/token');
+
 module.exports = {
     verify: (req, res, next) => {
-      res.status(404).json({
-        statusCode: 404,
-        status: "Not found."
-      });
+      tokenModule.verify(req.body.token)
+        .then((result) => {
+          res.status(200).json({
+            isVaild: result
+          });
+        }, (rejectTask) => {
+          let resMes = rejectTask;
+          resMes.debug = req.debug;
+          res.status(500).json(resMes);
+        })
+      ;
     },
     refresh: (req, res, next) => {
       res.status(404).json({
