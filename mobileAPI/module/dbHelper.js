23a45804e424e599eab921d5f38fc5628df6ea67
Erickson Juang <a820224@gmail.com>
2016-09-22 02:21:11 +0800

    force to using charset - utf8_general_ci

--- mobileAPI/module/dbHelper.js
+++ mobileAPI/module/dbHelper.js
@@ -9,7 +9,8 @@ module.exports.query = (query, params, cb) => {
 		password: __mobileAPIConfig.database.password,
 		database: __mobileAPIConfig.database.dbname,
 		multipleStatements: true,
-		timezone: '0000'
+		timezone: '0000',
+		charset: 'utf8_general_ci'
 	});
 
 	database.connect()
