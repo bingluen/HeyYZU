bbca27b9a3511103aa40e4ff48ffce69ddfdc346
Erickson Juang <a820224@gmail.com>
2016-09-22 00:44:08 +0800

    referrals user/course with detail option

--- mobileAPI/controller/v3/userController.js
+++ mobileAPI/controller/v3/userController.js
@@ -3,17 +3,31 @@ const v2Referrals = require(__mobileAPIBase + 'module/v3/v2Referrals');
 
 module.exports = {
   getCourses: (req, res, next) => {
-    v2Referrals.userCourse({token: req.query.token})
-      .then((resolve) => {
-        if (resolve.httpStatus === 200) {
-          res.status(200).json(resolve.data.map((cv) => cv.lesson_id));
-        } else {
-          res.status(resolve.httpStatus).json({
-            statusCode: resolve.statusCode,
-            status: resolve.status
-          });
-        }
-      });
+    if (req.query.current === "true") {
+      v2Referrals.courseInfo({token: req.query.token})
+        .then((resolve) => {
+          if (resolve.httpStatus === 200) {
+            res.status(200).json(resolve.data);
+          } else {
+            res.status(resolve.httpStatus).json({
+              statusCode: resolve.statusCode,
+              status: resolve.status
+            });
+          }
+        });
+    } else {
+      v2Referrals.userCourse({token: req.query.token})
+        .then((resolve) => {
+          if (resolve.httpStatus === 200) {
+            res.status(200).json(resolve.data.map((cv) => cv.lesson_id));
+          } else {
+            res.status(resolve.httpStatus).json({
+              statusCode: resolve.statusCode,
+              status: resolve.status
+            });
+          }
+        });
+    }
   },
   refreshCourse: (req, res, next) => {
     v2Referrals.userRefreshCourse(req.body)
