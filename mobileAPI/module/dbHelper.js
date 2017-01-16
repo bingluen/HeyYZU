d327e05dd3b169f6f12db45b1d449e984affff2a
Erickson Juang <a820224@gmail.com>
2017-01-16 17:57:23 +0800

    add transaction(promise)  & promise query method

--- mobileAPI/module/dbHelper.js
+++ mobileAPI/module/dbHelper.js
@@ -1,18 +1,16 @@
 var mysql = require('mysql');
-
+var dbPool = mysql.createPool({
+	host: __mobileAPIConfig.database.host,
+	user: __mobileAPIConfig.database.username,
+	password: __mobileAPIConfig.database.password,
+	database: __mobileAPIConfig.database.dbname,
+	multipleStatements: true,
+	timezone: '0000',
+	charset: 'utf8_general_ci'
+});
 
 
 module.exports.query = (query, params, cb) => {
-	var dbPool = mysql.createPool({
-		host: __mobileAPIConfig.database.host,
-		user: __mobileAPIConfig.database.username,
-		password: __mobileAPIConfig.database.password,
-		database: __mobileAPIConfig.database.dbname,
-		multipleStatements: true,
-		timezone: '0000',
-		charset: 'utf8_general_ci'
-	});
-
 	dbPool.getConnection(function(err, connection) {
 	  // Use the connection
 	  connection.query(query, params, function(err, result, field) {
@@ -26,3 +24,62 @@ module.exports.query = (query, params, cb) => {
 		sql: 'deprecated'
 	};
 };
+
+module.exports.transaction = (query, params) => {
+	return new Promise((resolve, reject) => {
+		db.pool.getConnection((err, connection) => {
+
+			if (err) { reject(err); } // get connection failed
+			
+			else {
+				connection.beginTransaction((err) => {
+
+					if (err) {
+						reject(err); // begin transaction failed
+						connection.release(); // release connection
+					} else {
+						connection.query(query, params, (err, result, field) => {
+							if (err) {
+								connection.rollback(() => { // do query failed
+									reject(err)
+									connection.release(); // release connection
+								});
+							} else {
+								connection.commit((err) => {
+									if (err) {
+										connection.rollback(() => { // do commit failed
+											reject(err);
+											connection.release(); // release connection
+										});
+									} else {
+										resolve(result); // transaction successful
+										connection.release(); // release connection
+									}
+								});
+							}
+						});
+					}
+
+				});
+			}
+		});
+	});
+}
+
+module.exports.queryPromise = (query, params) => {
+	return new Promise((resolve, reject) => {
+		dbPool.getConnection(function(err, connection) {
+		  // Use the connection
+		  connection.query(query, params, function(err, result, field) {
+		    // And done with the connection.
+		    if (err) {
+					resolve(result);
+				} else {
+					reject(err);
+				}
+		    connection.release();
+		    // Don't use the connection here, it has been returned to the pool.
+		  });
+		});
+	})
+}
